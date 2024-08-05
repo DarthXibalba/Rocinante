71269e18e58778881577e238afa54b9627ce0428
Javier Urquizu <javier.urquizu@hp.com>
2024-08-05 15:39:24 -0700

    Reformatted cmd/root.go (linux & windows)

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   runRocinanteBinary,
+	Short: "Rocinante is a CLI tool for use image manipulation.",
+	Long:  `Rocinante is a lightweight CLI tool use for a variety of image processing and image manipulation purposes.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+}
--- cmd/root_linux.go
+++ cmd/root_linux.go
@@ -2,23 +2,6 @@
 
 package cmd
 
-import (
-	"github.com/spf13/cobra"
+const (
+	runRocinanteBinary = "./Rocinante"
 )
-
-var rootCmd = &cobra.Command{
-	Use:   "./Rocinante",
-	Short: "Rocinante is a CLI tool for use image manipulation.",
-	Long:  `Rocinante is a lightweight CLI tool use for a variety of image processing and image manipulation purposes.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
-
-func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-}
